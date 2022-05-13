bd823eab0353f6faac130221332ecd9cd7d97964
Hux <droshux@gmail.com>
2022-05-13 12:49:21 +0100

    Completely re-did the animation/frames stuff :)

--- src/main/java/io/github/droshux/rigidbodies/Program.java
+++ src/main/java/io/github/droshux/rigidbodies/Program.java
@@ -10,13 +10,5 @@ public class Program {
     public static void main(String[] args) {
         Canvas = new CanvasTemplate();
         RigidBody r1 = new RigidBody("test1", 5, new Point(0,0), Color.BLUE, "mesh2.txt");
-        SwingUtilities.invokeLater(() -> {
-            /*for (int i = 0; i < 5000; i++) {
-                Canvas.repaint();
-                Canvas.Update(i);
-            }*/
-            r1.Rotate(Math.toRadians(45));
-            Canvas.repaint();
-        });
     }
 }
