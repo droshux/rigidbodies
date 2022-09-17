a6ec58d4bfe10ad43d443cdd5014a6eab5eb6786
droshux <droshux@gmail.com>
2022-09-17 23:15:36 +0100

    Changed main to test RigidBody.contains properly. Seems to be running smoothly...

--- src/main/java/io/github/droshux/rigidbodies/Program.java
+++ src/main/java/io/github/droshux/rigidbodies/Program.java
@@ -1,39 +1,46 @@
 package io.github.droshux.rigidbodies;
 
-import java.awt.*;
-
 public class Program {
 
-    public static void main(String[] args) {
-        Canvas Canvas = new Canvas(640, 480, 10, 3);
-//        Canvas.displayTime = true;
-        RigidBody rb1 = new RigidBodyBuilder(Canvas)
-                .setId("test1")
-                .setMass(5)
-                .setPosition(new Point(0, 0))
-                .setColour(Color.BLUE)
-                .setColliderFile("complex")
-                .setGravity(true)
-                .setElasticity(1)
-                .setRigidity(0)
-                .createRigidBody();
-        rb1.Forces.add(new RigidBody.LocalForce(new Vector(10, 0),
-                new Point(-1, -1),
-                0.1));
-        RigidBody floor = new RigidBodyBuilder(Canvas)
-                .setId("floor")
-                .setMass(1000)
-                .setPosition(new Point(0, -5))
-                .setColour(Color.DARK_GRAY)
-                .setGravity(false)
-                .setColliderFile("floor")
-                .createRigidBody();
-        //Canvas.start();
-        System.out.println(floor.contains(Utils.VectorAdd(new Vector(floor.Collider[0].points[0]), new Vector(0.2, 0.2))));
-        System.out.println(floor.contains(Utils.VectorAdd(new Vector(floor.Collider[0].points[0]), new Vector(-0.2, -0.2))));
+        public static void main(String[] args) {
+                Canvas Canvas = new Canvas(640, 480, 10, 3);
+                // Canvas.displayTime = true;
+                /*
+                 * RigidBody rb1 = new RigidBodyBuilder(Canvas)
+                 * .setId("test1")
+                 * .setMass(5)
+                 * .setPosition(new Point(0, 0))
+                 * .setColour(Color.BLUE)
+                 * .setColliderFile("complex")
+                 * .setGravity(true)
+                 * .setElasticity(1)
+                 * .setRigidity(0)
+                 * .createRigidBody();
+                 * rb1.Forces.add(new RigidBody.LocalForce(new Vector(10, 0),
+                 * new Point(-1, -1),
+                 * 0.1));
+                 * RigidBody rbTest = new RigidBodyBuilder(Canvas)
+                 * .setId("floor")
+                 * .setMass(1000)
+                 * .setPosition(new Point(0, -5))
+                 * .setColour(Color.DARK_GRAY)
+                 * .setGravity(false)
+                 * .setColliderFile("floor")
+                 * .createRigidBody();
+                 */
 
-        Triangle t1 = new Triangle(new Point(-1, -1), new Point(1, -1), new Point(0, 2));
-        System.out.println(t1.contains(new Point(0, 0)));
-        System.out.println(t1.contains(new Point(0, -2)));
-    }
+                RigidBody rbTest = new RigidBodyBuilder(Canvas)
+                                .setId("testBox")
+                                .setPosition(new Point(0, 0))
+                                .setColliderFile("mesh1")
+                                .createRigidBody();
+
+                // Canvas.start();
+                System.out.println(rbTest.contains(new Point(0, 0)));
+                System.out.println(rbTest.contains(new Point(10, 10)));
+
+                Triangle t1 = new Triangle(new Point(-1, -1), new Point(1, -1), new Point(0, 2));
+                System.out.println(t1.contains(new Point(0, 0)));
+                System.out.println(t1.contains(new Point(0, -2)));
+        }
 }
--- src/main/java/io/github/droshux/rigidbodies/RigidBody.java
+++ src/main/java/io/github/droshux/rigidbodies/RigidBody.java
@@ -6,11 +6,12 @@ import java.util.List;
 
 public class RigidBody {
     public String id;
-    public  Triangle[] Collider;
-    public Point[] BoundingBox = new Point[2]; //Axis aligned bounding box in world space. [0] is bottom left and [1] is top right
+    public Triangle[] Collider;
+    public Point[] BoundingBox = new Point[2]; // Axis aligned bounding box in world space. [0] is bottom left and [1]
+                                               // is top right
     public Point Position;
-    public double Mass; //In kilograms
-    public final double MomentOfInertia; //kgm^2
+    public double Mass; // In kilograms
+    public final double MomentOfInertia; // kgm^2
     public final boolean UseGravity;
     public Color Colour;
     public double elasticity;
@@ -19,44 +20,62 @@ public class RigidBody {
     public Canvas canvas;
 
     public List<LocalForce> Forces = new ArrayList<>();
-    public Vector Velocity = new Vector(); public Vector Acceleration = new Vector();
+    public Vector Velocity = new Vector();
+    public Vector Acceleration = new Vector();
     public double AngularVelocity = 0;
     public LocalForce Weight;
 
-    public RigidBody(String id, float mass, Point position, Color col, String colliderFile, Canvas canvasTemplate, boolean useGravity, double elasticity, double rigidity) {
-        this.id = id; this.Mass = mass; this.Position = position; this.Colour = col; this.canvas = canvasTemplate; this.UseGravity=useGravity; this.elasticity = elasticity; this.rigidity = rigidity;
+    public RigidBody(String id, float mass, Point position, Color col, String colliderFile, Canvas canvasTemplate,
+            boolean useGravity, double elasticity, double rigidity) {
+        this.id = id;
+        this.Mass = mass;
+        this.Position = position;
+        this.Colour = col;
+        this.canvas = canvasTemplate;
+        this.UseGravity = useGravity;
+        this.elasticity = elasticity;
+        this.rigidity = rigidity;
         this.Collider = Utils.getMeshFromFile(colliderFile);
 
         Point COM = CenterOfMass();
-        //Translate all coordinates so that 0,0 is COM
-        for (Triangle t : Collider) for (Point p : t.points) {
-            p.x -= COM.x;
-            p.y -= COM.y;
-        }
-        if (UseGravity) Weight = new LocalForce(Utils.VectorScalarMultiply(Utils.g, Mass), new Point(0,0), 1);
+        // Translate all coordinates so that 0,0 is COM
+        for (Triangle t : Collider)
+            for (Point p : t.points) {
+                p.x -= COM.x;
+                p.y -= COM.y;
+            }
+        if (UseGravity)
+            Weight = new LocalForce(Utils.VectorScalarMultiply(Utils.g, Mass), new Point(0, 0), 1);
         MomentOfInertia = calculateI();
         canvas.Objects.add(this);
     }
 
     private double calculateI() {
         List<Point> polygon = new ArrayList<>();
-        for (Triangle t : Collider) polygon.addAll(Arrays.asList(t.points)); //Make a list of all points in the mesh
-        polygon = Utils.removeDuplicates(polygon); //Remove duplicates
+        for (Triangle t : Collider)
+            polygon.addAll(Arrays.asList(t.points)); // Make a list of all points in the mesh
+        polygon = Utils.removeDuplicates(polygon); // Remove duplicates
         double Jx = 0;
-        for (int i = 0; i <= polygon.size()-1; i++) {
+        for (int i = 0; i <= polygon.size() - 1; i++) {
             int iPlus = i + 1;
-            if (iPlus >= polygon.size()) iPlus = 0; //Loop round
-            Point Pi = polygon.get(i); Point PiPlus = polygon.get(iPlus); //Get two points
-            Jx += ((Pi.x * PiPlus.y)-(PiPlus.x * Pi.y)) * (Math.pow(Pi.x, 2) + (Pi.x * PiPlus.x) + Math.pow(PiPlus.x, 2));
+            if (iPlus >= polygon.size())
+                iPlus = 0; // Loop round
+            Point Pi = polygon.get(i);
+            Point PiPlus = polygon.get(iPlus); // Get two points
+            Jx += ((Pi.x * PiPlus.y) - (PiPlus.x * Pi.y))
+                    * (Math.pow(Pi.x, 2) + (Pi.x * PiPlus.x) + Math.pow(PiPlus.x, 2));
         }
         Jx /= 12;
 
         double Jy = 0;
-        for (int i = 0; i <= polygon.size()-1; i++) {
+        for (int i = 0; i <= polygon.size() - 1; i++) {
             int iPlus = i + 1;
-            if (iPlus >= polygon.size()) iPlus = 0; //Loop round
-            Point Pi = polygon.get(i); Point PiPlus = polygon.get(iPlus); //Get two points
-            Jy += ((Pi.x * PiPlus.y)-(PiPlus.x * Pi.y)) * (Math.pow(Pi.y, 2) + (Pi.y * PiPlus.y) + Math.pow(PiPlus.y, 2));
+            if (iPlus >= polygon.size())
+                iPlus = 0; // Loop round
+            Point Pi = polygon.get(i);
+            Point PiPlus = polygon.get(iPlus); // Get two points
+            Jy += ((Pi.x * PiPlus.y) - (PiPlus.x * Pi.y))
+                    * (Math.pow(Pi.y, 2) + (Pi.y * PiPlus.y) + Math.pow(PiPlus.y, 2));
         }
         Jy /= 12;
 
@@ -65,14 +84,18 @@ public class RigidBody {
 
     public Point CenterOfMass() {
         List<Point> MeshPoints = new ArrayList<>();
-        for (Triangle t : Collider) MeshPoints.addAll(Arrays.asList(t.points)); //Make a list of all points in the mesh
-        MeshPoints = Utils.removeDuplicates(MeshPoints); //Remove duplicates
-        float totalX = 0; float totalY = 0; //Calculate mean...
+        for (Triangle t : Collider)
+            MeshPoints.addAll(Arrays.asList(t.points)); // Make a list of all points in the mesh
+        MeshPoints = Utils.removeDuplicates(MeshPoints); // Remove duplicates
+        float totalX = 0;
+        float totalY = 0; // Calculate mean...
         for (Point point : MeshPoints) {
-            totalX += point.x; totalY += point.y;
+            totalX += point.x;
+            totalY += point.y;
         }
-        totalX /= MeshPoints.size(); totalY /= MeshPoints.size();
-        return new Point(totalX, totalY); //Return :)
+        totalX /= MeshPoints.size();
+        totalY /= MeshPoints.size();
+        return new Point(totalX, totalY); // Return :)
     }
 
     public void Rotate(double theta) {
@@ -85,32 +108,41 @@ public class RigidBody {
     }
 
     @SuppressWarnings("unused")
-    public Point LocalToWorldSpace(Point p) {return new Point(this.Position.x + p.x, this.Position.y + p.y);}
+    public Point LocalToWorldSpace(Point p) {
+        return new Point(this.Position.x + p.x, this.Position.y + p.y);
+    }
+
     @SuppressWarnings("unused")
-    public Point WorldToLocalSpace(Point p) {return new Point(p.x-this.Position.x, p.y-this.Position.y);}
+    public Point WorldToLocalSpace(Point p) {
+        return new Point(p.x - this.Position.x, p.y - this.Position.y);
+    }
 
     @SuppressWarnings("unused")
     public void RotateAboutPoint(Point point, double theta) {
-        for (Triangle t : Collider) for (Point p : t.points) {
-            p.x -= point.x;
-            p.y -= point.y;
-        }
+        for (Triangle t : Collider)
+            for (Point p : t.points) {
+                p.x -= point.x;
+                p.y -= point.y;
+            }
         Rotate(theta);
-        for (Triangle t : Collider) for (Point p : t.points) {
-            p.x += point.x;
-            p.y += point.y;
-        }
+        for (Triangle t : Collider)
+            for (Point p : t.points) {
+                p.x += point.x;
+                p.y += point.y;
+            }
     }
 
     public void Update(double delta) {
-        if (!Forces.contains(Weight) && UseGravity) Forces.add(Weight);
+        if (!Forces.contains(Weight) && UseGravity)
+            Forces.add(Weight);
         Acceleration = new Vector();
         Iterator<LocalForce> i = Forces.iterator();
         while (i.hasNext()) {
             LocalForce F = i.next();
             AddForceAtPosition(F, delta);
             F.duration -= delta;
-            if (F.duration <= 0) i.remove();
+            if (F.duration <= 0)
+                i.remove();
         }
         Velocity = Utils.VectorAdd(Velocity, Acceleration);
         Position = Utils.VectorAdd(new Vector(Position), Utils.VectorScalarMultiply(Velocity, delta));
@@ -120,26 +152,33 @@ public class RigidBody {
 
     @SuppressWarnings("unused")
     public void AddForceAtPosition(Vector force, Point localPosition, double delta) {
-        Acceleration = Utils.VectorAdd(Acceleration, Utils.VectorScalarMultiply(force, delta/Mass));
-        double Torque = localPosition.x * force.y - localPosition.y * force.x; //2D torque calculation
+        Acceleration = Utils.VectorAdd(Acceleration, Utils.VectorScalarMultiply(force, delta / Mass));
+        double Torque = localPosition.x * force.y - localPosition.y * force.x; // 2D torque calculation
         AngularVelocity += Torque * delta / MomentOfInertia;
     }
 
     public void AddForceAtPosition(LocalForce F, double delta) {
-        Acceleration = Utils.VectorAdd(Acceleration, Utils.VectorScalarMultiply(F.forceVector, delta/Mass));
-        double Torque = F.localPosition.x * F.forceVector.y - F.localPosition.y * F.forceVector.x; //2D torque calculation
+        Acceleration = Utils.VectorAdd(Acceleration, Utils.VectorScalarMultiply(F.forceVector, delta / Mass));
+        double Torque = F.localPosition.x * F.forceVector.y - F.localPosition.y * F.forceVector.x; // 2D torque
+                                                                                                   // calculation
         AngularVelocity += Torque * delta / MomentOfInertia;
     }
 
     private void UpdateBoundingBox() {
-        Point InitialPoint = Collider[0].points[0]; //First Point to be tested
-        double minX = InitialPoint.x, maxX = InitialPoint.x, minY = InitialPoint.y, maxY = InitialPoint.y; //Initialise min and max
+        Point InitialPoint = Collider[0].points[0]; // First Point to be tested
+        double minX = InitialPoint.x, maxX = InitialPoint.x, minY = InitialPoint.y, maxY = InitialPoint.y; // Initialise
+                                                                                                           // min and
+                                                                                                           // max
 
-        for (Triangle t : Collider) for (Point p : t.points) {
-            minX = Math.min(p.x, minX); maxX = Math.max(p.x, maxX);
-            minY = Math.min(p.y, minY); maxY = Math.max(p.y, maxY);
-        }
-        BoundingBox[0] = LocalToWorldSpace(new Point(minX, minY)); BoundingBox[1] = LocalToWorldSpace(new Point(maxX, maxY));
+        for (Triangle t : Collider)
+            for (Point p : t.points) {
+                minX = Math.min(p.x, minX);
+                maxX = Math.max(p.x, maxX);
+                minY = Math.min(p.y, minY);
+                maxY = Math.max(p.y, maxY);
+            }
+        BoundingBox[0] = LocalToWorldSpace(new Point(minX, minY));
+        BoundingBox[1] = LocalToWorldSpace(new Point(maxX, maxY));
     }
 
     public boolean contains(Point p) {
@@ -147,18 +186,25 @@ public class RigidBody {
 
         for (Triangle t : this.Collider) {
             output = t.contains(WorldToLocalSpace(p));
-            System.out.println("CONTAINS" + output);
-            if (output) break;
+            // System.out.println("CONTAINS" + output);
+            if (output)
+                break;
         }
 
-        return  output;
+        return output;
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof RigidBody rigidBody)) return false;
-        return Double.compare(rigidBody.Mass, Mass) == 0 && Double.compare(rigidBody.MomentOfInertia, MomentOfInertia) == 0 && UseGravity == rigidBody.UseGravity && Double.compare(rigidBody.elasticity, elasticity) == 0 && Double.compare(rigidBody.rigidity, rigidity) == 0 && id.equals(rigidBody.id) && Colour.equals(rigidBody.Colour);
+        if (this == o)
+            return true;
+        if (!(o instanceof RigidBody rigidBody))
+            return false;
+        return Double.compare(rigidBody.Mass, Mass) == 0
+                && Double.compare(rigidBody.MomentOfInertia, MomentOfInertia) == 0 && UseGravity == rigidBody.UseGravity
+                && Double.compare(rigidBody.elasticity, elasticity) == 0
+                && Double.compare(rigidBody.rigidity, rigidity) == 0 && id.equals(rigidBody.id)
+                && Colour.equals(rigidBody.Colour);
     }
 
     @Override
@@ -172,7 +218,11 @@ public class RigidBody {
 
         public double duration;
 
-        public LocalForce(Vector forceVector, Point localPosition, double duration) {this.forceVector = forceVector; this.localPosition = localPosition; this.duration = duration;}
+        public LocalForce(Vector forceVector, Point localPosition, double duration) {
+            this.forceVector = forceVector;
+            this.localPosition = localPosition;
+            this.duration = duration;
+        }
 
         @Override
         public String toString() {
@@ -184,8 +234,10 @@ public class RigidBody {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
             LocalForce that = (LocalForce) o;
             return forceVector.equals(that.forceVector) && localPosition.equals(that.localPosition);
         }
